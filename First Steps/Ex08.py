bcb642068dffdc22c871a18a5d73341f7c9cf542
viniciuslizarte <lizarte.vls@gmail.com>
2020-06-01 14:14:15 -0300

    Add files via upload

---
+++ First Steps/Ex08.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+
+print('Programa de conversão de medidas, Metros, Centimetros e Milimetros!')
+m = int(input('Escreva um valor em metros: '))
+print('{}Metros equivale a {}Centimetros e a {}Milimetros' .format(m, m*100, m*1000))
+
+
